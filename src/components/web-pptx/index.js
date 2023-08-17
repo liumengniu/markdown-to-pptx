38022f87cbc7be5d176b9f7705e3c39a14991c3e
liumengniu <1*AveqrjUE5LlCHA0Gtj>
2023-08-17 15:57:51 +0800

    修改logo

--- src/components/web-pptx/index.js
+++ src/components/web-pptx/index.js
@@ -6,13 +6,14 @@
 import "./index.scss"
 // Import Swiper React components
 import {Swiper, SwiperSlide} from 'swiper/react';
-import { Navigation, Pagination } from "swiper";
+import {Navigation, Pagination} from "swiper";
 // Import Swiper styles
 import "swiper/css/navigation";
 // Import Swiper styles
 import 'swiper/css';
 import {useEffect} from "react";
 import utils from "@utils";
+import _ from "lodash";
 
 function WebPptx(props) {
 	const {markdownStr} = props
@@ -23,37 +24,67 @@ function WebPptx(props) {
 	
 	}, [])
 	
-	const renderCover = () =>{
-		return (<div style={{color:"#ffffff", fontSize: 24, fontWeight: 'bold'}}>
-			{_.get(mdTree, `0.text`)}
-		</div>)
+	/**
+	 * 渲染封面
+	 * @returns {JSX.Element}
+	 */
+	const renderCover = () => {
+		return (
+			<SwiperSlide>
+				<div className="cover-slide" style={{textAlign: "center",color: "#ffffff", fontSize: 24, fontWeight: 'bold'}}>
+					{_.get(mdTree, `0.text`)}
+				</div>
+			</SwiperSlide>
+		)
+	}
+	/**
+	 * 渲染全部slide
+	 */
+	const renderSlides = () => {
+		const slidesTree = _.get(mdTree, '0.children');
+		return (
+			<div className="other-slides">
+				{
+					_.map(slidesTree, (o, idx) => {
+						return (
+							<SwiperSlide key={idx}>
+								<div className={"slide-title"}>{o?.text}</div>
+								{
+									_.map(o?.children, (p,childIdx)=> {
+										return (
+											<div key={`${idx}-${childIdx}`} className={"slide-list-title"}>{p?.text}</div>
+										)
+								})
+								}
+							</SwiperSlide>
+						)
+					})
+				}
+			</div>
+		)
 	}
 	
 	/**
 	 * 渲染slider， API尽量和 pptxgenjs 贴近，保持渲染版和导出版差异最小
 	 */
-	const renderSlider = () =>{
+	const renderSlider = () => {
 	
 	}
 	
 	return (
 		<div className="web-pptx">
 			<Swiper
-				className="mySwiper"
+				className="swiper"
 				navigation={true}
 				pagination={{
 					type: 'fraction',
 				}}
-				modules={[Navigation,Pagination]}
+				modules={[Navigation, Pagination]}
 				onSlideChange={() => console.log('slide change')}
 				onSwiper={(swiper) => console.log(swiper)}
 			>
-				<SwiperSlide>
-					{renderCover()}
-				</SwiperSlide>
-				<SwiperSlide>Slide 2</SwiperSlide>
-				<SwiperSlide>Slide 3</SwiperSlide>
-				<SwiperSlide>Slide 4</SwiperSlide>
+				{renderCover()}
+				{renderSlides()}
 			</Swiper>
 		</div>
 	)
