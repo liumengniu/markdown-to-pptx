7d0341c9ad98b3730781dd07dc36b3384793fb00
liumengniu <1*AveqrjUE5LlCHA0Gtj>
2023-08-22 17:50:46 +0800

    修改logo

--- src/components/web-pptx/index.js
+++ src/components/web-pptx/index.js
@@ -11,7 +11,7 @@ import {Navigation, Pagination} from "swiper";
 import "swiper/css/navigation";
 // Import Swiper styles
 import 'swiper/css';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import utils from "@utils";
 import _ from "lodash";
 
@@ -21,11 +21,9 @@ let list = [];
 function WebPptx(props) {
 	const {rightData} = props
 	
-
 	useEffect(() => {
 		renderAllSlide()
 	}, [])
-	
 	/**
 	 * 展平树结构 -> pptx所需数组
 	 * @param data
@@ -33,12 +31,10 @@ function WebPptx(props) {
 	const flattenTree = data =>{
 		_.map(data, o=>{
 			if(o.type==="section"){
-				flattenTree(o?.children)
-			} else {
 				list.push(o)
+				flattenTree(o?.children)
 			}
 		})
-		// console.log(list, 'listlistlistlistlistlistlistlistlistlistlistlist')
 		return list
 	}
 	/**
@@ -46,52 +42,26 @@ function WebPptx(props) {
 	 */
 	const renderAllSlide = () => {
 		let pptxData = flattenTree(rightData)
-		console.log(rightData, '===========================rightData========================', pptxData)
-		html = renderCoverAndDirectory(_.get(rightData, `0`));
-		if(!_.isEmpty(rightData)){
-			return renderSlide(rightData, html)
-		} else {
-			return null
-		}
-	}
-	/**
-	 * 渲染幻灯片的html
-	 * @param tree
-	 * @param oldHtml
-	 * @returns {null}
-	 */
-	const renderSlide2 = (tree, oldHtml) => {
-		// let html = null;
-		// let newHtml = null;
-		_.map(tree, o => {
-			if (o.level && o.type === "section" && o.level === 1) {  //渲染封面和目录
-				html = renderCoverAndDirectory(o)
-			} else {  //渲染除封面/目录外的幻灯片（PS：只渲染至倒数第二级）
-				// html = (!_.isEmpty(o.children) && o.type !== "list") ? renderChildSlide(o, oldHtml) : null
-				// html = (!_.isEmpty(o.children) && o.type !== "list") ? renderChildSlide(o, oldHtml) : null
-				// html = !_.isEmpty(o.children) ? renderChildSlide(o, oldHtml) : null
-				html = renderChildSlide(o, oldHtml)
-			}
-			if (!_.isEmpty(o.children) && o.type !== "list") {
-				// renderSlide(o.children, html)
-				html = renderSlide(o.children, html)
-			}
-			return o;
-		})
-		console.log(html, 'htmlhtmlhtmlhtmlhtmlhtmlhtmlhtmlhtmlhtmlhtml')
-		return html;
-	}
-	
-	const renderSlide = (tree, oldHtml) => {
-		_.map(tree, o=>{
-			if(!_.isEmpty(o?.children)){
-				html += renderSlide(o?.children, oldHtml)
-			} else {
-				// html += renderChildSlide(o, oldHtml)
-			}
-			return o;
-		})
-		return html
+		return (
+			<>
+				{
+					_.map(pptxData, o=>{
+						return (
+							<SwiperSlide>
+								<div className="common-slide">
+									<h2>{o?.text}</h2>
+									<div>
+										{_.map(_.get(o, `children`), p=>{
+											return <div key={o?.id}>{p?.text}</div>
+										})}
+									</div>
+								</div>
+							</SwiperSlide>
+						)
+					})
+				}
+			</>
+		)
 	}
 	/**
 	 * 渲染封面&目录
@@ -120,29 +90,6 @@ function WebPptx(props) {
 			</>
 		)
 	}
-	/**
-	 * 渲染子级幻灯片
-	 */
-	const renderChildSlide = (item, oldHtml) => {
-		console.log("执行了多少次renderChildSlide==")
-		return (
-			<>
-				{oldHtml}
-				<SwiperSlide>
-					<div className="common-slide">
-						<h2>{item?.text}</h2>
-						<div>
-							{_.map(_.get(item, `children`), o=>{
-								return <div key={o?.id}>{o?.text}</div>
-							})}
-						</div>
-					</div>
-				</SwiperSlide>
-			</>
-		)
-	}
-
-	// console.log(html, 'htmlhtmlhtmlhtmlhtmlhtmlhtmlhtmlhtmlhtml')
 
 	return (
 		<div className="web-pptx">
@@ -156,18 +103,7 @@ function WebPptx(props) {
 				onSlideChange={() => console.log('slide change')}
 				onSwiper={(swiper) => console.log(swiper)}
 			>
-				{/*<SwiperSlide>*/}
-				{/*	<div className="cover-slide" style={{textAlign: "center", color: "#ffffff", fontSize: 24, fontWeight: 'bold'}}>*/}
-				{/*		{"eeee"}*/}
-				{/*	</div>*/}
-				{/*</SwiperSlide>*/}
-				{/*{renderCoverAndDirectory()}*/}
-				{/*{renderCover()}*/}
-				{/*{renderSlides()}*/}
-				
-				{/*{renderSlide(rightData)}*/}
-				
-				{html}
+				{renderAllSlide()}
 			</Swiper>
 		</div>
 	)
